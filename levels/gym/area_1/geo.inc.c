fd7acd92856b3294617147e014a5dbff9aef2fe1
Doumty <126972846+Doumty@users.noreply.github.com>
2026-08-09 19:39:36 +0200

    THERE IS TREES IN MY GAME AHHHH
    *ELTY HELP WHAT IS GOING ON THERE IS TREES IN THE ROMHACK

--- levels/gym/area_1/geo.inc.c
+++ levels/gym/area_1/geo.inc.c
@@ -3,6 +3,7 @@
 const GeoLayout gym_area_1_geo[] = {
 	GEO_NODE_START(),
 	GEO_OPEN_NODE(),
+		GEO_TRANSLATE_NODE_WITH_DL(LAYER_OPAQUE, 0, 100, -1900, gym_dl_Cube_mesh_layer_1),
 		GEO_DISPLAY_LIST(LAYER_OPAQUE, gym_dl_platform_mesh_layer_1),
 		GEO_DISPLAY_LIST(LAYER_OPAQUE, gym_dl_final_revert_mesh_layer_1),
 	GEO_CLOSE_NODE(),
--- levels/gym/script.c
+++ levels/gym/script.c
@@ -11,6 +11,8 @@
 
 #include "levels/scripts.h"
 
+#include "actors/group14.h"
+#include "actors/common1.h"
 #include "make_const_nonconst.h"
 #include "levels/gym/header.h"
 
@@ -26,14 +28,25 @@ const LevelScript level_gym_entry[] = {
 	ALLOC_LEVEL_POOL(),
 	MARIO(MODEL_MARIO, 0x00000001, bhvMario), 
 	JUMP_LINK(script_func_global_1), 
+	LOAD_MODEL_FROM_GEO(MODEL_BOB_BUBBLY_TREE, bubbly_tree_geo), 
 	/* Fast64 begin persistent block [level commands] */
 	/* Fast64 end persistent block [level commands] */
 
 	AREA(1, gym_area_1),
 		WARP_NODE(0x0A, LEVEL_BOB, 0x01, 0x0A, WARP_NO_CHECKPOINT),
+		OBJECT(MODEL_BUTTERFLY, -700, 0, -900, 0, 0, 0, 0x00000000, bhvButterfly),
+		OBJECT(MODEL_BOB_BUBBLY_TREE, -1500, 0, -500, 0, 0, 0, 0x00000000, bhvTree),
+		OBJECT(MODEL_SSL_PALM_TREE, -1100, 0, -500, 0, 0, 0, 0x00000000, bhvTree),
+		OBJECT(MODEL_SL_SNOW_TREE, -700, 0, -500, 0, 0, 0, 0x00000000, bhvTree),
+		OBJECT(MODEL_CASTLE_DOOR_0_STARS, 1200, 0, -1600, 0, 0, 0, 0x00000000, bhvDoor),
+		OBJECT(MODEL_BBH_HAUNTED_DOOR, 1200, 0, -1200, 0, 0, 0, 0x00000000, bhvDoor),
+		OBJECT(MODEL_CASTLE_METAL_DOOR, 1200, 0, -800, 0, 0, 0, 0x00000000, bhvDoor),
+		OBJECT(MODEL_CASTLE_WOODEN_DOOR, 1200, 0, -400, 0, 0, 0, 0x00000000, bhvDoor),
+		OBJECT(MODEL_YELLOW_COIN, 2100, 0, 2000, 0, 0, 0, 0x00000000, bhvYellowCoin),
+		OBJECT(MODEL_RED_COIN, 2100, 0, 1600, 0, 0, 0, 0x00000000, bhvRedCoin),
 		TERRAIN(gym_area_1_collision),
 		MACRO_OBJECTS(gym_area_1_macro_objs),
-		SET_BACKGROUND_MUSIC(0x00, SEQ_LEVEL_GRASS),
+		SET_BACKGROUND_MUSIC(0x00, SEQ_EVENT_CUTSCENE_CREDITS),
 		TERRAIN_TYPE(TERRAIN_GRASS),
 		/* Fast64 begin persistent block [area commands] */
 		/* Fast64 end persistent block [area commands] */
