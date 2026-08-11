9f4cf424f476f8972cd8f4a06055f4b4760251df
Doumty <126972846+Doumty@users.noreply.github.com>
2026-08-12 00:51:45 +0200

    WIP castle

--- levels/welkit_castle_past/script.c
+++ levels/welkit_castle_past/script.c
@@ -21,7 +21,7 @@
 const LevelScript level_welkit_castle_past_entry[] = {
 	INIT_LEVEL(),
 	LOAD_YAY0(0x07, _welkit_castle_past_segment_7SegmentRomStart, _welkit_castle_past_segment_7SegmentRomEnd), 
-	LOAD_YAY0(0x0A, _bidw_skybox_yay0SegmentRomStart, _bidw_skybox_yay0SegmentRomEnd), 
+	LOAD_YAY0(0x0A, _water_skybox_yay0SegmentRomStart, _water_skybox_yay0SegmentRomEnd), 
 	LOAD_YAY0(0x08, _common0_yay0SegmentRomStart, _common0_yay0SegmentRomEnd), 
 	LOAD_RAW(0x0F, _common0_geoSegmentRomStart, _common0_geoSegmentRomEnd), 
 	ALLOC_LEVEL_POOL(),
@@ -34,25 +34,25 @@ const LevelScript level_welkit_castle_past_entry[] = {
 	LOAD_MODEL_FROM_GEO(MODEL_BBH_HAUNTED_DOOR, haunted_door_geo), 
 	LOAD_MODEL_FROM_GEO(MODEL_CASTLE_METAL_DOOR, metal_door_geo), 
 	LOAD_MODEL_FROM_GEO(MODEL_CASTLE_WOODEN_DOOR, wooden_door_geo), 
+	LOAD_MODEL_FROM_GEO(MODEL_CASTLE_DOOR_0_STARS, castle_door_0_star_geo), 
+	LOAD_MODEL_FROM_GEO(MODEL_CASTLE_KEY_DOOR, key_door_geo), 
 	/* Fast64 begin persistent block [level commands] */
 	/* Fast64 end persistent block [level commands] */
 
 	AREA(1, welkit_castle_past_area_1),
 		WARP_NODE(0x0A, LEVEL_BOB, 0x01, 0x0A, WARP_NO_CHECKPOINT),
-		OBJECT(MODEL_BUTTERFLY, -700, 0, -900, 0, 0, 0, 0x00000000, bhvButterfly),
-		OBJECT(MODEL_CASTLE_GROUNDS_WARP_PIPE, 0, 0, -600, 0, 0, 0, 0x00000000, bhvWarpPipe),
 		OBJECT(MODEL_BOB_BUBBLY_TREE, -1500, 0, -500, 0, 0, 0, 0x00000000, bhvTree),
 		OBJECT(MODEL_SSL_PALM_TREE, -1100, 0, -500, 0, 0, 0, 0x00000000, bhvTree),
 		OBJECT(MODEL_SL_SNOW_TREE, -700, 0, -500, 0, 0, 0, 0x00000000, bhvTree),
 		OBJECT(MODEL_CASTLE_DOOR_0_STARS, 1200, 0, -1600, 0, 0, 0, 0x00000000, bhvDoor),
 		OBJECT(MODEL_BBH_HAUNTED_DOOR, 1200, 0, -1200, 0, 0, 0, 0x00000000, bhvDoor),
+		OBJECT(MODEL_CASTLE_KEY_DOOR, -1689, 539, 3630, 0, 90, 0, 0x00000000, bhvDoor),
+		OBJECT(MODEL_CASTLE_KEY_DOOR, -1689, 539, 3480, 0, -90, 0, 0x00000000, bhvDoor),
 		OBJECT(MODEL_CASTLE_METAL_DOOR, 1200, 0, -800, 0, 0, 0, 0x00000000, bhvDoor),
 		OBJECT(MODEL_CASTLE_WOODEN_DOOR, 1200, 0, -400, 0, 0, 0, 0x00000000, bhvDoor),
-		OBJECT(MODEL_YELLOW_COIN, 0, 11300, 0, 0, 0, 0, 0x00000000, bhvYellowCoin),
-		OBJECT(MODEL_RED_COIN, 0, 11300, 0, 0, 0, 0, 0x00000000, bhvRedCoin),
 		TERRAIN(welkit_castle_past_area_1_collision),
 		MACRO_OBJECTS(welkit_castle_past_area_1_macro_objs),
-		SET_BACKGROUND_MUSIC(0x00, SEQ_EVENT_CUTSCENE_CREDITS),
+		SET_BACKGROUND_MUSIC(0x00, SEQ_LEVEL_INSIDE_CASTLE),
 		TERRAIN_TYPE(TERRAIN_GRASS),
 		/* Fast64 begin persistent block [area commands] */
 		/* Fast64 end persistent block [area commands] */
