d5ed37bbac58dbf1e5e19196117598f7f9230591
Doumty <126972846+Doumty@users.noreply.github.com>
2026-08-12 13:52:41 +0200

    Hub castle beta is done for now
    * Hub castle is done
    * Fixed collision problems in the hub

--- levels/welkit_castle_past/area_1/macro.inc.c
+++ levels/welkit_castle_past/area_1/macro.inc.c
@@ -5,6 +5,11 @@ const MacroObject welkit_castle_past_area_1_macro_objs[] = {
 	MACRO_OBJECT_WITH_BEH_PARAM(macro_yellow_coin, 0, -1693, 1308, 2759, 0x00000000),
 	MACRO_OBJECT_WITH_BEH_PARAM(macro_yellow_coin, 0, -1693, 1308, 3157, 0x00000000),
 	MACRO_OBJECT_WITH_BEH_PARAM(macro_yellow_coin, 0, -1693, 1308, 3954, 0x00000000),
+	MACRO_OBJECT_WITH_BEH_PARAM(macro_yellow_coin, 0, 1683, 1308, 4352, 0x00000000),
+	MACRO_OBJECT_WITH_BEH_PARAM(macro_yellow_coin, 0, 1683, 1308, 3555, 0x00000000),
+	MACRO_OBJECT_WITH_BEH_PARAM(macro_yellow_coin, 0, 1683, 1308, 2759, 0x00000000),
+	MACRO_OBJECT_WITH_BEH_PARAM(macro_yellow_coin, 0, 1683, 1308, 3157, 0x00000000),
+	MACRO_OBJECT_WITH_BEH_PARAM(macro_yellow_coin, 0, 1683, 1308, 3954, 0x00000000),
 	MACRO_OBJECT_END(),
 };
 
